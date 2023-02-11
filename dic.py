840e5e6b9586de07d4a269dfdf718e6da0923ad6
유경만/책임연구원/Q9디바이스개발Project(kyungman.yu@lge.com) <kyungman.yu@lge.com>
2023-02-11 18:50:18 +0900

    [add]randomForeset

--- dic.py
+++ dic.py
@@ -1,10 +1,14 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import plot_tree
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.linear_model import LinearRegression
+from sklearn import model_selection
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -52,14 +56,58 @@ print(xticks)
 tree = DecisionTreeRegressor().fit(X_train, y_train)
 tree_prediction = tree.predict(X_test)
 
-plt.xlabel("X-axis")
-plt.ylabel("Y-axis")
-plt.title("suction Plot")
-plt.plot(tree_prediction,color='green')
-# plt.plot(y_test,color='r')
+# plt.xlabel("X-axis")
+# plt.ylabel("Y-axis")
+# plt.title("suction predict treeprediction")
+# plt.plot(tree_prediction,color='green')
+# # plt.plot(y_test,color='r')
 
-plt.show()
+# plt.show()
 
 # print(tree_prediction)
 
+
+from sklearn.model_selection import train_test_split, GridSearchCV
+
+param_grid = {'n_estimators': [100, 200],
+              'oob_score': [True], # compute out of bag error
+              'n_jobs':[-1], 
+              'max_depth': [3, 5]
+              }
+
+
+# rf_model = RandomForestClassifier()
+rf_model = RandomForestRegressor()
+
+# hyperparameter search
+grid_search = GridSearchCV(rf_model, param_grid=param_grid, cv=5, scoring='f1')
+grid_search.fit( X_train, y_train)
+
+grid_search.best_params_
+
+opt_model = grid_search.best_estimator_
+opt_model
+
+
+test_pred_y = opt_model.predict(X_test)
+test_pred_y
+
+
+
+x = []
+for i in range(len(test_pred_y)):
+    x.append(i)
+mae = np.mean(np.abs(test_pred_y-y_test))
+print(mae)
+
+# plt.plot(y_test,color='r')
+
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("suction predict randomforest")
+# plt.plot(test_pred_y,y_test,color=['r','b'])
+plt.plot(x,test_pred_y,color='red')
+plt.plot(x,y_test,color='green')
+
+plt.show()
 
