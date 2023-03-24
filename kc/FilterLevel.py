289b39eeaa5ab8fa1885f268400f26ae0c38bbd7
kyuchun.choi <kcmanse@gmail.com>
2023-03-24 12:13:33 +0900

    [update]latest code

--- kc/FilterLevel.py
+++ kc/FilterLevel.py
@@ -1,17 +1,17 @@
 import sklearn
 import pandas as pd
 import numpy as np
-from sklearn import model_selection # cross-validation score를 가져오기 위함
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
+# from sklearn import model_selection # cross-validation score를 가져오기 위함
+from sklearn.linear_model import LinearRegression, LogisticRegression
+# from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.ensemble import BaggingClassifier # bagging
-from sklearn.tree import DecisionTreeClassifier # 의사 결정 나무
-from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+# from sklearn.ensemble import BaggingClassifier # bagging
+# from sklearn.tree import DecisionTreeClassifier # 의사 결정 나무
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor, StackingRegressor, StackingClassifier
 from collections import Counter # count
-from sklearn.metrics import f1_score
-from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import f1_score, accuracy_score
+from sklearn.neural_network import MLPRegressor, MLPClassifier
 import xgboost as xgb
 import lightgbm as lgb
 
@@ -22,25 +22,34 @@ import warnings
 warnings.simplefilter("ignore", UserWarning)
 
 # hyperparameters
-param_rf = {'n_estimators': [200],
+param_rf = {'n_estimators': [50,100,150,200],
               'oob_score': [True], # compute out of bag error
               'n_jobs':[-1], 
               'max_depth': [25, 50, 75] }
 
-param_xgb = {"max_depth": [25,50,75],
+param_xgb = {"max_depth": [25,50,75, 100],
               "min_child_weight" : [1,3,6],
-              "n_estimators": [200], 
-              "learning_rate": [0.05, 0.1,0.16]  }
+              "n_estimators": [100, 200, 300], 
+              "learning_rate": [0.01, 0.05, 0.1]  }
 
-param_lgb = { "objective":['regression'], # multiclass, regression
+param_lgb = { "objective":['multiclass'], # multiclass, regression
               "max_depth": [25,50, 75],
               "learning_rate" : [0.01,0.05,0.1],
               "num_leaves": [300,900,1200],
-              "n_estimators": [200]     }
+              "n_estimators": [100,200,300]     }
 
 train_file = './train_data.csv'
 test_file = './test_data.csv'
 
+Ensenmble_model = {'rf': [RandomForestClassifier(),RandomForestRegressor()], \
+                   'xgb': [xgb.XGBClassifier(),xgb.XGBRegressor()],\
+                   'lgb': [lgb.LGBMClassifier(), lgb.LGBMRegressor()]}
+
+Ensenmble_param = {'rf':[param_rf, param_rf], \
+                   'xgb':[param_xgb, param_xgb],\
+                   'lgb':[param_lgb, param_lgb]}
+
+trainning = {'Classifier':0, 'Regressor':1}   
 
 class AI_Filter_Level():
     def File_read(self, type):
@@ -58,20 +67,20 @@ class AI_Filter_Level():
     def Data_set(self, df, type, split = 0):       
         # df.pressure = np.log(df.pressure)
         # df.rpm = np.log(df.rpm)
-        print(df.head(5))        
         # df = df[df['mode'] == 2.0]
+        # df = df.drop(df[df.orifice == 30].index)
         
         # x 변수 선택
         # X = df[['mode', 'pressure', 'rpm', 'orifice']]
         X = df[['mode', 'pressure', 'rpm']]
-        # X = df[['pressure', 'rpm']]
+        # X = df[['pressure','rpm']]
         
         if type == 'suction':
             Y = df.suction
         elif type == 'orifice':    
             Y = df.orifice
-        elif type == 'level5':
-            Y = self.df.level5
+        elif type == 'level':
+            Y = df.level5
             
         # print(X[:5], Y[:5])
         
@@ -81,37 +90,44 @@ class AI_Filter_Level():
         else:
             return X, Y            
                       
-    def Trainning_ensenmble(self, type):
-        # 1) 모델 선언 & 2) 여러 모델들을 ensemble: randomforest
-        if type == 'rf':
-            model = RandomForestRegressor()
-            parm = param_rf
-        elif type == 'xgb':
-            model = xgb.XGBRegressor()
-            parm = param_xgb
-        elif type == 'lgb':
-            model = lgb.LGBMRegressor()     
-            parm = param_lgb  
+    def Trainning_ensenmble(self, model, type):
+        model_ensen = Ensenmble_model[model][trainning[type]]
+        parm = Ensenmble_param[model][trainning[type]]
         
-        # hyperparameter search
-        # self.grid_search = GridSearchCV(model, param_grid=parm, cv=5, scoring='f1')
-        self.grid_search = GridSearchCV(model, param_grid=parm, cv = 5,scoring="accuracy")
+        # hyperparameter search                
+        if type == 'Regressor': self.grid_search = GridSearchCV(model_ensen, param_grid=parm, cv = 25)
+        else:self.grid_search = GridSearchCV(model_ensen, param_grid=parm, cv=25, scoring = 'accuracy')
         
         self.grid_search.fit(self.train_x, self.train_y)        
         self.opt_model = self.grid_search.best_estimator_    
+        print(self.opt_model)
         
-    def Trainning_MLP(self):        
+    def Trainning_MLP(self, player = (4,4,2), type = 'Classifier'):   
+    # mode, pressure, rpm
+    #(5,4,3)(8,7,6)(12,9,7)(13,8,3)    
+    #(4,4,2)_classifier(5,4),(6,5)
+    # pressure, rpm, mode = 2 fix
+    #(5,2,2)(7,3,1)
+        
+        layer = player
         self.scaler = StandardScaler()
         # self.scaler = MinMaxScaler()
         self.scaler.fit(self.train_x)
         self.train_x = self.scaler.transform(self.train_x)
         
-        self.reg_mlp = MLPRegressor(activation='relu', alpha=0.001, batch_size=10,
-                    hidden_layer_sizes=(21,9,3), max_iter=10000, # 16,9,3
-                    solver='adam', verbose = True, random_state = 2023)
+        if type == 'Classifier':
+            self.reg_mlp = MLPClassifier(activation='relu', alpha=0.001, batch_size=10,
+                        hidden_layer_sizes=layer, max_iter=10000, # 16,9,3
+                        solver='adam', verbose = False, random_state = 2023)
+
+        elif type == 'Regressor':
+            self.reg_mlp = MLPRegressor(activation='relu', alpha=0.001, batch_size=10,
+                        hidden_layer_sizes=layer, max_iter=10000, # 16,9,3
+                        solver='adam', verbose = False, random_state = 2023)
         
         self.reg_mlp.fit(self.train_x, self.train_y)     
     
+        return self.reg_mlp
         train_loss_values = self.reg_mlp.loss_curve_        
         # print(self.reg_mlp.coefs_)
         
@@ -122,18 +138,30 @@ class AI_Filter_Level():
         plt.title("Learning Curve of trained MLP Regressor", fontsize=18)
         plt.show()        
         
-    def Testing(self, type):
-        if type == 'ensemble':
+    def Testing(self, model, type, view = True):
+        if model == 'ensemble':
             test_pred_y = self.opt_model.predict(self.test_x)
-        elif type == 'MLP':
+            train_pred_y = self.opt_model.predict(self.train_x)
+        elif model == 'MLP':
             self.test_x = self.scaler.transform(self.test_x)
             test_pred_y = self.reg_mlp.predict(self.test_x)
+            train_pred_y = self.reg_mlp.predict(self.train_x)
             
-        mae = np.mean(np.abs(test_pred_y-self.test_y))
-        mape = np.mean(np.abs(test_pred_y-self.test_y)/self.test_y)
+        if type == 'Classifier':
+            acc_score_test =accuracy_score(y_true=self.test_y, y_pred=test_pred_y)
+            print('acc_score_test',acc_score_test)
+            acc_score_train =accuracy_score(y_true=self.train_y, y_pred=train_pred_y)            
+            print('acc_score_train',acc_score_train)         
+               
+        elif type == 'Regressor':
+            mae = np.mean(np.abs(test_pred_y-self.test_y))
+            print('test mae:',mae)
+            mae = np.mean(np.abs(train_pred_y-self.train_y))
+            print('train mae:',mae)
+            # mape = np.mean(np.abs(test_pred_y-self.test_y)/self.test_y)
+            # print('mape:',mape)        
         
-        print('mae:',mae)
-        print('mape:',mape)
+        if view == False : return
         
         plt.figure(figsize = (16,8))
         ind = np.argsort(np.array(self.test_y))
@@ -142,27 +170,70 @@ class AI_Filter_Level():
         plt.legend()
         plt.title('suction power')
         plt.show()
+
+    def Optimal_MLP(self, type = 'Classifier'):        
+        # for d3 in range(3,30):
+            for d2 in range(2,30):
+                # if d2 > d3: continue
+                for d1 in range(1,30):       
+                    if d1 > d2: continue
+                    # layer = (d3,d2,d1)
+                    layer = (d2,d1)
+                    print(layer)
+                    self.Trainning_MLP(layer, type)    
+                    self.Testing('MLP', type, False)      
+
+    def stacking(self, type):        
+        if type == 'Regressor' :
+            #Stacking할 모델들 list로 정의
+            model_list = [('rf',RandomForestRegressor(max_depth=25, n_estimators=150, n_jobs=-1, oob_score=True)),
+                          ('xgb',xgb.XGBRegressor(max_depth=25, n_estimators=200, learning_rate=0.1,  min_child_weight=6)),
+                          ('lgb',lgb.LGBMRegressor(max_depth=25, n_estimators=300, num_leaves=300, objective='regression'))]
+            #Final model 정의
+            model_final = LinearRegression()
+            #Stacking 모델 정의
+            model = StackingRegressor(estimators=model_list,final_estimator=model_final,cv=25)
+        else :            
+            model_list = [('rf',RandomForestClassifier(max_depth=50, n_estimators=200, n_jobs=-1, oob_score=True)),  # mode, pressure, rpm                          
+                           ('xgb',xgb.XGBClassifier(max_depth=25, n_estimators=200, learning_rate=0.05,  min_child_weight=1)),
+                           ('lgb',lgb.LGBMClassifier(learning_rate=0.05, max_depth=25, num_leaves=300, objective='multiclass'))]            
+            model_final = LogisticRegression()
+            model = StackingClassifier(estimators=model_list,final_estimator=model_final,cv=5)       
         
-    def Check_result_var(self):
-        self.opt_model = self.grid_search.best_estimator_
-        print(self.opt_model)        
-        print(self.opt_model.feature_importances_)
-        print(self.opt_model.oob_score_)
-        print(self.grid_search.best_params_)      
+        #Stacking 모델 학습
+        model.fit(self.train_x,self.train_y)
+        
+        self.opt_model = model          
+        
+    def save_data(self):
+        test_pred_y = self.opt_model.predict(self.test_x)      
+        train_pred_y = self.opt_model.predict(self.train_x)     
         
     def Processing(self):
-        df_train = self.File_read('train')
-        self.train_x, self.train_y = self.Data_set(df_train, 'suction', 0)
-        # self.Trainning_ensenmble('rf') # model : rf, xgb , lgb
-        self.Trainning_MLP() 
+        trainning_target = 'level' # level, suction
         
+        if trainning_target == 'level': trainning_type = 'Classifier'
+        else : trainning_type = 'Regressor'        
+        
+        df_train = self.File_read('train')
         df_test = self.File_read('test')
-        self.test_x, self.test_y = self.Data_set(df_test, 'suction', 0)
-        # self.Testing('ensemble')  
-        self.Testing('MLP')               
+        
+        # df_data = pd.concat([df_train, df_test], axis=0)
+        # print(df_data.shape)
+        
+        self.train_x, self.train_y = self.Data_set(df_train, trainning_target, 0)
+        self.test_x, self.test_y = self.Data_set(df_test, trainning_target, 0)
+        
+        # self.Trainning_ensenmble('rf', trainning_type) # model : rf, xgb , lgb
+        # self.stacking(trainning_type)
+        # self.Trainning_MLP(trainning_type)        
+        self.Optimal_MLP()              
+        
+        # self.Testing('ensemble', trainning_type)  
+        # self.Testing('MLP', trainning_type)               
         
         print('finish')
-        
+     
 oAI = AI_Filter_Level()
 oAI.Processing()
 
